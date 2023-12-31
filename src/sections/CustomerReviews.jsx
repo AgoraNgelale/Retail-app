1b8c78cf7cc5bebf8a51b8a2b03decb7797f0583
Agora Ngelale <agorangelale@Agoras-MacBook-Air-3.local>
2023-12-31 00:26:08 -0800

    customer reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { reviews } from '../constants'
+import ReviewCard from '../components/ReviewCard'
 
 const CustomerReviews = () => {
   return (
-    <div>
-      CustomerReviews
+   <section className='max-container'>
+    <h3 className='font-palanquin text-center text-4xl font-bold'>
+      What Do Our
+      <span className='text-coral-red'> Customers </span>
+      Think?
+    </h3>
+    <p className='info-text m-auto 
+    mt-4 max-w-lg text-center'> 
+      Hear genuine stories from our satisfied customers about their
+      exceptional experiences with us.
+    </p>
+    <div className='mt-24 flex flex-1 justify-evenly
+    max-lg:flex-col gap-14 items-center'>
+      {reviews.map((review)=>(
+        <ReviewCard 
+          key={review.customerName}
+          imgURL = {review.imgURL}
+          customerName = {review.customerName}
+          rating = {review.rating}
+          feedback = {review.feedback}
+        />
+      ))}
     </div>
+   </section>
   )
 }
 
