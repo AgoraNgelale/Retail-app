1b588ff3e4d5689bc819ded416b4ceb405468c86
Agora Ngelale <agorangelale@Agoras-MacBook-Air-3.local>
2023-12-31 01:02:44 -0800

    subscribe

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Button = ({label, iconURL,backgroundColor, borderColor, textColor}) => {
+const Button = ({label, iconURL,backgroundColor, borderColor, textColor,fullWidth}) => {
   return (
     <button className={`${backgroundColor? backgroundColor
     :"bg-coral-red"} ${borderColor? borderColor :"bg-coral-red"}
     ${textColor? textColor : "text-white"}
-    rounded-full border
+    rounded-full ${fullWidth && 'w-full'} border
     flex justify-center items-center
      px-7 py-4 gap-2 text-lg leading-none font-montserrat`}>
         {label}
